7c90ac139ff728f0f7df0859a82ff119a1a5ba8b
JRM <johnrhmartin@gmail.com>
2022-03-31 23:55:55 -0400

    Created directories for index.html and notes.html via res.sendFile

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true })); // takes incoming POST data an converts it to key/value pairings
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public')); // all of our front-end code can be access without having a specific server endpoint created
 
 
 const { notes } = require('./data/notes');
@@ -80,13 +81,20 @@ app.get('/api/notes/:id', (req, res) => {
     res.json(result);
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
 app.post('/api/notes', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = notes.length.toString();
 
     // if any data in req.body is incorrect, send 400 error back
     if (!validateNote(req.body)) {
-        res.status(400).send('The note is not properly formatted.');
+        res.status(400).send('The note is not properly formatted.'); // response method to relay a message to the client making the request
     } else {
         // add notes to json file and notes array in this function
         const note = createNewNote(req.body, notes);
