f4909046e2e6ba5fa1c01348f6cbd1719f363e32
JRM <johnrhmartin@gmail.com>
2022-03-31 21:51:51 -0400

    initial push, built server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const { notes } = require('./data/notes');
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.listen(3001, () => {
+    console.log(`API Server now on port 3001!`);
+});
