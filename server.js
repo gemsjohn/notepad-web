6f755eb3fc336b1e452f58371d7679b1b6b22665
JRM <johnrhmartin@gmail.com>
2022-03-31 22:29:34 -0400

    Add Heroku

--- server.js
+++ server.js
@@ -3,8 +3,34 @@ const app = express();
 
 const { notes } = require('./data/notes');
 
+function filterByQuery(query, notesArray) {
+    let noteTextArray = [];
+    let filteredResults = notesArray;
+    if (query.noteText) {
+        if (typeof query.noteText === 'string') {
+            noteTextArray = [query.noteText];
+        } else {
+            noteTextArray = query.noteText;
+        }
+        noteTextArray.forEach(notes => {
+            filteredResults = filteredResults.filter(note => note.noteText.indexOf(text) !== -1);
+        });
+    }
+    if (query.title) {
+        filteredResults = filteredResults.filter(note => note.title === query.title);
+    }
+    if (query.text) {
+        filteredResults = filteredResults.filter(note => note.text === query.text)
+    }
+    return filteredResults;
+}
+
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
 });
 
 app.listen(3001, () => {
